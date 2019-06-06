8d99c28009316f488d779b74f364fe758cadd940
naveenmurugesan <naveenmurugesan4@gmail.com>
2019-06-06 10:50:32 +0530

    Update two.py

--- two.py
+++ two.py
@@ -1,5 +1,5 @@
-a=int(input("Enter the lower limit for the range:"))
-b=int(input("Enter the upper limit for the range:"))
-for i in range(a,b+1):
-    if(i%2!=0):
-        print(i)
+a=int(input("Enter the lower limit :"))
+b=int(input("Enter the upper limit :"))
+for x in range(a,b+1):
+    if(x%2!=0):
+        print(x)
