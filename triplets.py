11fcc7bb5f75847d67e00bbd7383a392c54af3ad
naveenmurugesan <naveenmurugesan4@gmail.com>
2019-06-08 09:47:55 +0530

    Update triplets.py

--- triplets.py
+++ triplets.py
@@ -4,6 +4,6 @@ count=0
 for x in range(len(l)-2):
     for y in range(x+1,len(l)-1):
         for z in range(y+1,len(l)):
-            if l[x]<l[y<l[z]and x<y<z
+            if l[x]<l[y]<l[z]and x<y<z
                 count=count+1
 print(count)
