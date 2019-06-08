ea69bb68b83cc8f415485abd77b43beb696ca1cf
naveenmurugesan <naveenmurugesan4@gmail.com>
2019-06-08 09:45:34 +0530

    Create triplets.py

---
+++ triplets.py
@@ -0,0 +1,9 @@
+n=int(input())
+l=list(map(int,input().split()))
+count=0
+for x in range(len(l)-2):
+    for y in range(x+1,len(l)-1):
+        for z in range(y+1,len(l)):
+            if l[x]<l[y<l[z]and x<y<z
+                count=count+1
+print(count)
