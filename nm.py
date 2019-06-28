f50ddd1b8b6e1df0b34cc4e51ddb7bd9d05974d9
naveenmurugesan <naveenmurugesan4@gmail.com>
2019-06-28 17:49:55 +0530

    Update nm.py

--- nm.py
+++ nm.py
@@ -4,5 +4,5 @@ for x in range (1,m):
     a[x]+=a[x-1]
 for x in range (n):
     s,t=map(int,input().split())
-    y=a[t-1] if s==1 else a[t-1]-a[s-2]
-    print(y)
+    z=a[t-1] if s==1 else a[t-1]-a[s-2]
+    print(z)
