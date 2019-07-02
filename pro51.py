c510e38fad32af40cec40f8e9a9ec6bef15453fe
naveenmurugesan <naveenmurugesan4@gmail.com>
2019-07-02 15:50:40 +0530

    Update pro51.py

--- pro51.py
+++ pro51.py
@@ -1,21 +1,21 @@
-def check(ll):
+def check(l):
         c=1
-        for jj in range(0,llen(ll)-1):
-                if ll[jj]!=ll[jj+1]:
+        for v in range(0,len(l)-1):
+                if l[v]!=l[v+1]:
                         c=c+1
                 else:
                     break
         return c
 num=int(input())
-ll=llist(map(int,input().spllit()))
-for jj in range(0,llen(ll)):
-        if ll[jj]>0:
-                ll[jj]=1
+l=list(map(int,input().split()))
+for v in range(0,len(l)):
+        if l[v]>0:
+                l[v]=1
         else:
-             ll[jj]=0
+             l[v]=0
 s=""
-for jj in range(0,llen(ll)):
-        k=ll[jj::]
+for v in range(0,len(l)):
+        k=l[v::]
         s=s+str(check(k))+" "
 print(s.strip())
              
