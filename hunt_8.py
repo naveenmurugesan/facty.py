5d28a3b0a2f0cf871ec5f0e3a503e9db2d8aa5c5
naveenmurugesan <naveenmurugesan4@gmail.com>
2019-07-03 17:52:20 +0530

    Update hunt_8.py

--- hunt_8.py
+++ hunt_8.py
@@ -2,6 +2,6 @@ p=int(input())
 l=list(map(int,input().split()))
 for i in range(0,p-2):
     for j in range(i+1,p-1):
-        for k in range(j+1,p1):
+        for k in range(j+1,p):
             if(l[i]+l[j]==l[k]):
                 print(l[i], l[j], l[k])
